0845d8c6e03e491db48308063fd6fd8d954db4cd
Yasmine Mardak <yasminemardak145@gmail.com>
2026-05-29 16:20:58 +0200

    Correction de la classe Triangle

--- src/Model/Triangle.java
+++ src/Model/Triangle.java
@@ -2,4 +2,54 @@ package Model;
 
 public class Triangle {
 
+    private Point a;
+    private Point b;
+    private Point c;
+
+    /**
+     * Constructs a triangle with three points.
+     *
+     * @param a first point
+     * @param b second point
+     * @param c third point
+     */
+
+    public Triangle(Point a, Point b, Point c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public Point getA() {
+        return a;
+    }
+
+    public Point getB() {
+        return b;
+    }
+
+    public Point getC() {
+        return c;
+    }
+
+    public void setA(Point a) {
+        this.a = a;
+    }
+
+    public void setB(Point b) {
+        this.b = b;
+    }
+
+    public void setC(Point c) {
+        this.c = c;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle{" +
+                "a=" + a +
+                ", b=" + b +
+                ", c=" + c +
+                '}';
+    }
 }
